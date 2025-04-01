b21f49018f8fd384b4cf1ccd21938153ecff9b7c
zakstanyer <zakstanyer@icloud.com>
2025-04-01 11:19:49 +0100

    Updates to projects page More content on Zombie Rush

---
+++ Models/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace my_blazor_project.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        //max length = 50
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? Title { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+
+        //limit the genre to just characters A-Z, as well as brackets, spaces and dashes
+        [Required]
+        [StringLength (50, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
+        public string? Genre { get; set; }
+
+        //limit to 2 decimal place
+        [Range(0, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        
+        //score must be 10 or between 0 and 9, or N/A
+        [Required]
+        [RegularExpression(@"[1][0]|[0-9]|N/A")]
+        public string? Score { get; set; }
+    }
+}
